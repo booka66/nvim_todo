6ca35d1264a8059aa853d4db5c6c166f78f81132
booka66 <jacobbcahoon@gmail.com>
2024-05-26 00:51:57 -0600

    please

--- lua/todo.lua
+++ lua/todo.lua
@@ -478,6 +478,7 @@ function M.AddTodoItem()
 	local file_path = os.getenv("HOME") .. "/.todo.json"
 	local file = io.open(file_path, "r")
 	local todo_list = {}
+	local current_file = vim.api.nvim_buf_get_name(0)
 
 	if file then
 		local content = file:read("*all")
@@ -501,7 +502,7 @@ function M.AddTodoItem()
 			prompt = "Enter a new category: ",
 		}, function(category)
 			if category then
-				AddTodoItemWithCategory(category)
+				AddTodoItemWithCategory(category, current_file)
 			else
 				print("No category entered.")
 			end
@@ -551,9 +552,9 @@ function M.AddTodoItem()
 			if input then
 				local selected_category = keys[input:lower()]
 				if selected_category then
-					AddTodoItemWithCategory(selected_category)
+					AddTodoItemWithCategory(selected_category, current_file)
 				else
-					AddTodoItemWithCategory(input)
+					AddTodoItemWithCategory(input, current_file)
 				end
 			else
 				print("No input provided.")
@@ -562,17 +563,16 @@ function M.AddTodoItem()
 	end
 end
 
-function M.AddTodoItemWithCategory(category)
+function M.AddTodoItemWithCategory(category, current_file)
 	vim.ui.input({
 		prompt = "Add item to '" .. category .. "':",
 	}, function(item)
 		if item then
 			local file_path = os.getenv("HOME") .. "/.todo.json"
 			local current_time = os.date("%Y-%m-%d %H:%M:%S")
-			local current_file = vim.fn.expand("%:t")
 			local todo_entry = {
 				item = item,
-				current_file = current_file ~= "" and current_file or nil,
+				current_file = current_file,
 				category = category,
 				created = current_time,
 				completed = false,
